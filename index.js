c7f3f571f2491fda1a310c75541b1ac4b4993bda
DahisC <dahischeng@gmail.com>
2020-08-24 07:37:18 +0800

    20200824

--- index.js
+++ index.js
@@ -1,26 +1,55 @@
-const fs = require('fs');
-const https = require('https');
-const AdmZip = require('adm-zip');
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const AdmZip = require("adm-zip");
+const readline = require("readline");
+
+const repo = "MarsFlyPig/PoDChineseUpdate";
 
 const headers = {
-  'User-Agent': 'PoD Chinese Localization Updater',
+  "User-Agent": "PoD Chinese Localization Updater",
 };
+
+const showApplicationInfo = () => {
+  console.log("PoD 中文化下載器 v1.0");
+  console.log("下載器作者：Dahis");
+  console.log("中文化作者：MarsFlyPig");
+  console.log("--------------------");
+};
+
+const checkInstallationPath = () => new Promise((resolve, reject) => {});
 
 const getLatestVersionInfo = () =>
   new Promise((resolve, reject) => {
-    let buffer = '';
+    console.log("[PoD 中文化下載器] - 正在取得最新的版本資訊 ...");
+    let buffer = "";
     https.get(
-      'https://api.github.com/repos/dahisc/release-test/releases/latest',
+      `https://api.github.com/repos/${repo}/releases/latest`,
       { headers },
       (res) => {
-        console.log(res.headers['content-length']);
-        res.on('data', (chunk) => {
+        res.on("data", (chunk) => {
           buffer += chunk;
         });
-        res.on('end', () => {
-          resolve(JSON.parse(buffer));
+        res.on("end", () => {
+          const versionInfo = JSON.parse(buffer);
+          console.log("[PoD 中文化下載器] 成功取得版本資訊");
+          console.log("\n");
+          console.log("-------------------------");
+          console.log("版本：");
+          console.log(`v${versionInfo.tag_name}`);
+          console.log("\n");
+          console.log("更新日期：");
+          console.log(new Date(versionInfo.published_at).toLocaleDateString());
+          console.log("\n");
+          console.log("更新內容：");
+          console.log(versionInfo.body);
+          console.log("-------------------------");
+          console.log("\n");
+
+          resolve(versionInfo);
         });
-        res.on('error', (err) => {
+        res.on("error", (err) => {
+          console.log("[PoD 中文化下載器] 失敗，無法取得最新中文化版本");
           reject(error);
         });
       }
@@ -29,29 +58,36 @@ const getLatestVersionInfo = () =>
 
 const downloadLatestFile = (latestVersion) =>
   new Promise((resolve, reject) => {
+    console.log("[PoD 中文化下載器] - 正在下載最新版本的中文化 ...");
     https.get(
-      `https://codeload.github.com/DahisC/release-test/zip/${latestVersion}`,
+      `https://codeload.github.com/${repo}/zip/${latestVersion}`,
       { headers },
       (res) => {
-        const ws = fs.createWriteStream(__dirname + '/123.zip');
+        // const ws = fs.createWriteStream(__dirname + '/456.zip');
+        const ws = fs.createWriteStream(path.join("./", `1.zip`));
         res.pipe(ws);
-        ws.on('finish', () => {
-          resolve('Download succeeded.');
+        ws.on("finish", () => {
+          console.log("[PoD 中文化下載器] 檔案下載完成");
+          resolve("Download succeeded.");
         });
-        ws.on('error', () => {
-          reject('Download failed.');
+        ws.on("error", (err) => {
+          console.log(err);
+          console.log("[PoD 中文化下載器] 檔案下載失敗");
+          reject("Download failed.");
         });
       }
     );
   });
 
-const uncompressZipFile = () =>
+const uncompressZipFile = (latestVersion) =>
   new Promise(async (resolve, reject) => {
-    const zip = new AdmZip('./123.zip');
+    console.log("[PoD 中文化下載器] - 正在安裝中文化 ...");
+    const zip = new AdmZip(path.join("./", `1.zip`));
     const zipEntries = zip.getEntries();
     zipEntries.forEach((entry) => {
-      if (entry.entryName.split('/')[1] === 'data' && entry.isDirectory) {
-        zip.extractEntryTo(entry, __dirname + '/data', false, true);
+      if (entry.entryName.split("/")[1] === "data" && entry.isDirectory) {
+        zip.extractEntryTo(entry, path.join("./", "data"), false, true);
+        console.log("[PoD 中文化下載器] 安裝完成");
       }
     });
   });
@@ -60,8 +96,12 @@ const execute = async () => {
   const versionInfo = await getLatestVersionInfo();
   const latestVersion = versionInfo.tag_name;
   await downloadLatestFile(latestVersion);
-  await uncompressZipFile();
-  //fs.unlink(__dirname + '/123.zip');
+  await uncompressZipFile(latestVersion);
+  readline
+    .createInterface(process.stdin, process.stdout)
+    .question("Press [Enter] to exit...", function () {
+      //process.exit();
+    });
 };
 
 execute();
