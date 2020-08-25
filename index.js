8c978db6fc4aab081bd8434254edd940d5f2844d
DahisC <dahischeng@gmail.com>
2020-08-26 00:55:46 +0800

    20200826

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const child_process = require("child_process");
 
 const repo = "MarsFlyPig/PoDChineseUpdate";
 
+let latestVersion;
+let localVersion;
+
 const headers = {
   "User-Agent": "PoD Chinese Localization Updater",
 };
@@ -65,6 +68,7 @@ const getLatestVersionInfo = () =>
           console.log("\n");
 
           fileName = fileName + "-" + versionInfo.tag_name;
+          latestVersion = versionInfo.tag_name;
 
           resolve(versionInfo);
         });
@@ -75,25 +79,42 @@ const getLatestVersionInfo = () =>
     );
   });
 
-const compareVersion = (latestVersion) =>
+const compareVersion = () =>
   new Promise((resolve, reject) => {
     console.log("[PoD 中文化下載器] - 正在檢查本地端中文化版本 ...");
-    fs.readdir("./", (err, files) => {
-      if (!files.includes("version.json")) {
-        console.log(
-          "[PoD 中文化下載器] 無法確認本地端版本，開始自動更新至最新版本"
-        );
 
+    fs.readdir("./", (err, files) => {
+      console.log(files);
+      if (!files.includes("data")) {
+        console.log("[PoD 中文化下載器] 尚未安裝中文化，開始安裝");
         resolve();
       } else {
-        fs.readFile("version.json", (err, data) => {
-          console.log(err, data);
+        fs.readdir("./data", (err, files) => {
+          if (!files.includes("version.json")) {
+            console.log(
+              "[PoD 中文化下載器] 無法確認本地端版本，開始自動更新至最新版本"
+            );
+
+            resolve();
+          } else {
+            fs.readFile("version.json", (err, data) => {
+              const localVersion = JSON.parse(data.toString()).version;
+              if (localVersion === latestVersion) {
+                console.log(
+                  `[PoD 中文化下載器] 本地端版本 ${localVersion}，最新版本 ${latestVersion}，版本不相符，開始自動更新`
+                );
+                resolve();
+              } else {
+                reject("已安裝最新版本");
+              }
+            });
+          }
         });
       }
     });
   });
 
-const downloadLatestFile = (latestVersion) =>
+const downloadLatestFile = () =>
   new Promise((resolve, reject) => {
     console.log("[PoD 中文化下載器] - 正在下載最新版本的中文化 ...");
     https.get(
@@ -113,20 +134,25 @@ const downloadLatestFile = (latestVersion) =>
     );
   });
 
-const uncompressZipFile = (latestVersion) =>
+const uncompressZipFile = () =>
   new Promise(async (resolve, reject) => {
     console.log("[PoD 中文化下載器] - 正在解壓縮中文化檔案 ...");
     const zip = new AdmZip(path.join("./", `${fileName}.zip`));
-    const zipEntries = zip.getEntries();
-    zipEntries.forEach((entry) => {
-      console.log(entry.entryName);
-      if (entry.entryName === fileName + "/data/" && entry.isDirectory) {
-        zip.extractEntryTo(entry, path.join("./"), true, true);
-        console.log("[PoD 中文化下載器] 解壓縮完成"); ///////////////////////////////寫道這
-        // fs.unlink(`${fileName}.zip`);
-        resolve();
-      }
-    });
+    zip.extractEntryTo(
+      `${fileName}/data/`,
+      path.join(__dirname + "/data"),
+      true,
+      true
+    );
+    // const zipEntries = zip.getEntries();
+    // zipEntries.forEach((entry) => {
+    //   if (entry.entryName === fileName + "/data/" && entry.isDirectory) {
+    //     zip.extractEntryTo(entry.entryName, path.join(__dirname), true, true);
+    //     console.log("[PoD 中文化下載器] 解壓縮完成"); ///////////////////////////////寫道這
+    //     // fs.unlink(`${fileName}.zip`);
+    //     resolve();
+    //   }
+    // });
   });
 
 const showExtraInfo = () => {
@@ -174,9 +200,9 @@ const execute = async () => {
     await checkInstallationPath();
     const versionInfo = await getLatestVersionInfo();
     const latestVersion = versionInfo.tag_name;
-    await compareVersion(latestVersion);
-    await downloadLatestFile(latestVersion);
-    await uncompressZipFile(latestVersion);
+    await compareVersion();
+    await downloadLatestFile();
+    await uncompressZipFile();
     // openLauncher();
     showExtraInfo();
     console.log("\n");
@@ -185,13 +211,11 @@ const execute = async () => {
     );
   } catch (err) {
     console.log("\n");
-    console.error("[PoD 中文化下載器] ***** 警告，安裝已被中止 *****");
+    console.error("[PoD 中文化下載器] ***** 安裝並未完成 *****");
     console.log("\n");
     console.log("原因：", err);
     console.log("\n");
-    await askQuestion(
-      "[PoD 中文化下載器] 安裝失敗QQ。\n\n按下 Enter 退出安裝程序 ..."
-    );
+    await askQuestion("[PoD 中文化下載器] \n\n按下 Enter 退出安裝程序 ...");
   }
 };
 
